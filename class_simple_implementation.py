667bb49e5f40d9c4d22c806e230cd7ce939749b8
stuti-pathak <stuti.pathak@bridgelabz.com>
2022-01-22 12:35:21 +0530

    Added: class simple demo

--- class_simple_implementation.py
+++ class_simple_implementation.py
@@ -47,19 +47,6 @@ class UserDetails:
         else:
             raise ContactCustomException("Name must be of at least 3 characters")
 
-    # @property
-    # def last_name(self):
-    #     return self.last_name_
-    #
-    # @last_name.setter
-    # def last_name(self, value):
-    #     if value is None or value is "":
-    #         raise ContactCustomException("Please enter valid name")
-    #     if re.fullmatch("^[A-Z]{1}[a-z]{2,}$", value):
-    #         self.last_name_ = value
-    #     else:
-    #         raise ContactCustomException("Name must be of at least 3 characters")
-
     def __str__(self):
         print("__str__() called")
         return self.first_name + " " + self.last_name
@@ -76,15 +63,6 @@ except Exception as exp:
 # print(UserDetails.get_name())
 name=input("enter the name")
 print(type(int(name)))
-# print(user1.first_name)
-# user1.first_name="Ammy"
-# print(user1.first_name)
-# print(user1)
-# user1.__str__()
-# try:
-#     user1.first_name = "joE"
-# except ContactCustomException as exception:
-#     print(exception.__str__())
 
 # print(UserDetails.__doc__)
 # print(UserDetails.first_name.__doc__)
